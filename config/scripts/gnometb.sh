74ce8da722a4cddbadfaed5ba0b21174f700617a
kityr <84194133+kityr@users.noreply.github.com>
2024-04-15 15:38:02 +0300

    Update and rename example.sh to gnometb.sh

--- config/scripts/gnometb.sh
+++ config/scripts/gnometb.sh
@@ -6,5 +6,4 @@
 set -oue pipefail
 
 # Your code goes here.
-echo 'This is an example shell script'
-echo 'Scripts here will run during build if specified in recipe.yml'
+rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:trixieua:mutter-patched gnome-shell mutter mutter-common xorg-x11-server-Xwayland gdm
