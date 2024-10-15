7585af0ac110f3b4b7f076779c657a1431b03745
kityr <84194133+kityr@users.noreply.github.com>
2024-10-15 15:28:43 +0300

    Create waveterm.sh

---
+++ files/scripts/waveterm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+latest_release=$(curl -s https://api.github.com/repos/wavetermdev/waveterm/releases/latest | grep -oP '"browser_download_url": "\K[^"]+' | grep 'waveterm-linux-x86_64.*\.rpm$')
+rpm-ostree install $latest_release
