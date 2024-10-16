c94c2dabf1f17df210415ea6cddbe5c4c566c388
kityr <84194133+kityr@users.noreply.github.com>
2024-10-16 21:03:53 +0300

    Update waveterm.sh

--- files/scripts/waveterm.sh
+++ files/scripts/waveterm.sh
@@ -6,3 +6,4 @@
 set -oue pipefail
 
 latest_release=$(curl -s https://api.github.com/repos/wavetermdev/waveterm/releases/latest | grep -oP '"browser_download_url": "\K[^"]+' | grep 'waveterm-linux-x86_64.*\.rpm$')
+echo $latest_release
